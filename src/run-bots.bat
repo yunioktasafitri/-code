31eb60cc0fda5e4db5df96f682855be26eb8442c
Randi <134947372+Ranzavia23@users.noreply.github.com>
2025-06-01 20:18:26 +0700

    Update run-bots

--- src/run-bots.bat
+++ src/run-bots.bat
@@ -1,5 +1,4 @@
 @echo off
-start cmd /c "python main.py --logic AlphaBot --email=test1@email.com --name=alphabot --password=123456 --team etimo"
+start cmd /c "python main.py --logic AlphaBot --email=alphacode@email.com --name=alphabot --password=123456 --team etimo"
 start cmd /c "python main.py --logic AlphaBot --email=test2@email.com --name=stima1 --password=123456 --team etimo"
 start cmd /c "python main.py --logic AlphaBot --email=test3@email.com --name=stima2 --password=123456 --team etimo"
-start cmd /c "python main.py --logic AlphaBot --email=test4@email.com --name=stima3 --password=123456 --team etimo"
